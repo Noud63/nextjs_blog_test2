a6babe5f56d77d079a6f03570c269a8d0002bf10
Noud63 <noudvandun@gmail.com>
2024-11-08 11:14:20 +0100

    refactor

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -9,11 +9,11 @@ const Menu = () => {
       <div className="grid grid-cols-3 gap-2 max-sm:hidden xl:grid-cols-6">
         {items &&
           items.map((item, index) => (
-            <Link href={item.href} key={index}>
+            <div key={index}>
               <div className="flex h-12 w-full cursor-pointer items-center justify-center rounded-lg border-2 text-white max-sm:h-10">
                 {item.title}
               </div>
-            </Link>
+            </div>
           ))}
       </div>
     </div>
