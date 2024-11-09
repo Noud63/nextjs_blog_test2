c2960db05ba302a004a03737b15e95a8d4ca4326
Noud63 <noudvandun@gmail.com>
2024-11-09 10:55:11 +0100

    comments size

--- components/PostComment.jsx
+++ components/PostComment.jsx
@@ -60,50 +60,51 @@ const PostComment = ({ post, comments }) => {
           Reacties:
         </div>
         {sortedComments.map((com, index) => (
-          <div key={index} className="flex h-auto w-full flex-row gap-2 px-4">
-            <div className="h-[45px] w-[50px] overflow-hidden rounded-full bg-gray-200">
-              <Image
-                src={
-                  com.userId?.avatar
-                    ? com.userId.avatar
-                    : "/images/defaultAvatar2.png"
-                }
-                alt="icon"
-                width={45}
-                height={0}
-                className="h-full w-full object-cover"
-              />
-            </div>
-
-            <div className="mb-4 flex w-full flex-col">
-              <div className="mb-1 flex flex-1 flex-col rounded-xl bg-gray-100 p-2">
-                <span className="text-sm font-semibold text-gray-800">
-                  {com.username}
-                </span>
-                <span>{com.comment}</span>
+          <div key={index} className="flex w-full flex-col">
+            <div className="flex w-full gap-2 px-4">
+              <div className="h-[45px] w-[50px] overflow-hidden rounded-full bg-gray-200">
+                <Image
+                  src={
+                    com.userId?.avatar
+                      ? com.userId.avatar
+                      : "/images/defaultAvatar2.png"
+                  }
+                  alt="icon"
+                  width={45}
+                  height={45}
+                  className="h-full w-full object-cover"
+                />
               </div>
 
-              <div className="flex justify-between text-[12px] font-normal text-gray-500">
-                <span className="mt-[4px] text-[11px]">
-                  Gepost: {`${new Date(com.createdAt).toLocaleDateString()}`}
-                </span>
-                <div className="flex flex-row items-center gap-2">
-                  {com.userId._id === session?.user?.id && (
+              <div className="flex w-full flex-col">
+                <div className="mb-1 flex flex-1 flex-col rounded-xl bg-gray-100 p-2">
+                  <span className="text-sm font-semibold text-gray-800">
+                    {com.username}
+                  </span>
+                  <span>{com.comment}</span>
+                </div>
+                <div className="flex justify-between text-[12px] font-normal text-gray-500 mt-1 mb-2">
+                  <span className="mt-[4px] text-[11px]">
+                    Gepost: {`${new Date(com.createdAt).toLocaleDateString()}`}
+                  </span>
+                  <div className="flex flex-row items-center gap-2">
+                    {com.userId._id === session?.user?.id && (
+                      <button
+                        type="button"
+                        className="flex cursor-pointer items-center pb-[1px] font-semibold text-gray-600"
+                        onClick={() => deleteComment(com._id)}
+                      >
+                        verwijder
+                      </button>
+                    )}
                     <button
                       type="button"
-                      className="flex cursor-pointer items-center pb-[1px] font-semibold text-gray-600"
-                      onClick={() => deleteComment(com._id)}
+                      className="flex w-[80px] cursor-pointer justify-center rounded-full border border-gray-500 text-[14px] font-semibold text-gray-600 max-xsm:w-[70px]"
+                      onClick={() => toggleLike(com._id)}
                     >
-                      verwijder
+                      Leuk {com.likesCount}
                     </button>
-                  )}
-                  <button
-                    type="button"
-                    className="flex w-[80px] cursor-pointer justify-center rounded-full border border-gray-500 text-[14px] font-semibold text-gray-600 max-xsm:w-[70px]"
-                    onClick={() => toggleLike(com._id)}
-                  >
-                    Leuk {com.likesCount}
-                  </button>
+                  </div>
                 </div>
               </div>
             </div>
