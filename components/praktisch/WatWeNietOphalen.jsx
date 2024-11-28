d6210dc231dc40daebf02ad7e1e6f75bada64bce
Noud63 <noudvandun@gmail.com>
2024-11-28 14:49:02 +0100

    grofvuil

--- components/praktisch/WatWeNietOphalen.jsx
+++ components/praktisch/WatWeNietOphalen.jsx
@@ -9,14 +9,14 @@ const WatWeNietOphalen = ({ dropDown }) => {
       }`}
     >
       <ul className="list-disc pl-8 pt-4">
-        <li>Bouw- en sloopafval</li>
+        <li>Bouw en sloopafval</li>
         <li>Karton</li>
-        <li>Klein chemisch afval, zoals verf en accu's</li>
+        <li>Klein chemisch afval, zoals verf en accu&#39;s</li>
         <li>Kleine elektrische apparaten</li>
         <li>Glasplaten en spiegels</li>
         <li>Tuintegels</li>
         <li>Autobanden</li>
-        <li>Auto-onderdelen</li>
+        <li>Auto onderdelen</li>
         <li>Overige motoren</li>
         <li>Stenen, zand en aarde</li>
       </ul>
