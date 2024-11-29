bc8587a33cea4a8292d97581b33fd5abdf7bae63
Noud63 <noudvandun@gmail.com>
2024-11-29 14:38:05 +0100

    dropdown menu

--- components/MenuOverlay.jsx
+++ components/MenuOverlay.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { items } from "../utils/menuItems";
 import { IoMdCloseCircleOutline } from "react-icons/io";
+import Image from "next/image";
 
 const MenuOverlay = ({ setOpenModal, openModal }) => {
   const closeModal = () => {
@@ -10,23 +11,34 @@ const MenuOverlay = ({ setOpenModal, openModal }) => {
 
   return (
     <div
-      className={`${openModal ? "translate-x-0" : "translate-x-full"} fixed bottom-0 right-0 top-0 z-[999] flex h-full max-h-screen w-full max-w-[500px] flex-col items-center justify-start overflow-y-auto bg-gradient-to-r from-white/70 via-white/50 to-white/70 p-4 backdrop-blur-lg transition duration-300 ease-in`}
+      className={`${openModal ? "translate-x-0" : "translate-x-full"} fixed bottom-0 right-0 top-0 z-[999] flex h-full max-h-screen w-full max-w-[500px] flex-col items-center justify-start overflow-y-auto bg-gradient-to-r from-red-950 via-yellow-700 to-red-950 p-4 backdrop-blur-lg transition duration-300 ease-in`}
       onClick={closeModal}
     >
       <div className="my-8 cursor-pointer" onClick={closeModal}>
-        <IoMdCloseCircleOutline size={35} color="#713f12" />
+        <IoMdCloseCircleOutline size={35} color="#fff" />
       </div>
       <div className="grid w-full grid-cols-2 gap-2 rounded-xl">
         {items &&
           items.map((item, index) => (
             <div key={index}>
               <Link href={item?.href || "/not-found"}>
-                <div className="flex h-12 w-full cursor-pointer items-center justify-center rounded-lg border-[3px] border-yellow-900 font-semibold tracking-wide text-yellow-900 shadow-lg">
+                <div className="flex h-12 w-full cursor-pointer items-center justify-center rounded-lg border-[3px] border-white font-semibold tracking-wide text-white shadow-lg">
                   {item.title}
                 </div>
               </Link>
             </div>
           ))}
+      </div>
+      <div className="flex w-full justify-center">
+        <div className="mt-12 flex h-[70px] w-[70px] items-center justify-center rounded-full border-2 pb-1 pl-[2px]">
+          <Image
+            src={"/images/logo.png"}
+            alt="logo"
+            width={100}
+            height={0}
+            className="h-[40px] w-[40px] object-cover rotate-6"
+          />
+        </div>
       </div>
     </div>
   );
