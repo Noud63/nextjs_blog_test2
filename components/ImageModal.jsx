4db4d5c12c92d0f481ce9deb4e693c79771e4a5a
Noud63 <noudvandun@gmail.com>
2024-12-03 15:06:23 +0100

    imagemodal

--- components/ImageModal.jsx
+++ components/ImageModal.jsx
@@ -8,15 +8,15 @@ const ImageModal = ({open, setOpen, image}) => {
         className="fixed bottom-0 left-0 right-0 top-0 z-[999] flex w-full items-center justify-center overflow-y-auto bg-yellow-950/80"
         onClick={() => setOpen(false)}
       >
-        <div className="h-auto w-[900px] border-t-8 border-x-8 border-white transition duration-500 ">
+        <div className="h-auto max-w-[900px] border-t-8 border-x-8 border-white transition duration-500 ">
           <Image
             src={image}
             width={900}
             height={0}
-            alt="lijn19"
+            alt="ovmap"
             className="h-auto w-auto cursor-pointer object-cover"
           />
-          <div className="bg-white text-yellow-900 py-4">Tramlijn 19</div>
+          <div className="bg-white text-yellow-700 text-lg py-4 font-semibold">{image === "/images/lijn19.png" ? "Tramlijn 19" : "Buslijn 21"}</div>
         </div>
       </div>
     )
