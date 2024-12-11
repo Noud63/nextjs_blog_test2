6cef250e85bc22ed005049bb0ef60779311163fe
Noud63 <noudvandun@gmail.com>
2024-12-11 13:20:32 +0100

    adressen

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,20 +18,24 @@ export const metadata = {
   }
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children}) {
+
+ 
   return (
     <html lang="en">
       <body className="relative bg-gradient-to-r from-red-950 via-yellow-700 to-red-950">
         <div className="fixed -z-10 h-full w-full bg-[url('../public/images/homebg.png')] bg-cover bg-center bg-no-repeat" />
         <AuthProvider>
-          <Navbar />
-          <div className="w-full max-sm:flex sm:hidden">
-            <LoginRegisterLogout />
-          </div>
+    
+            <Navbar />
+            <div className="w-full max-sm:flex sm:hidden">
+              <LoginRegisterLogout />
+            </div>
 
-          <Menu />
-          <main className="min-h-screen">{children}</main>
-          <Footer />
+            <Menu />
+            <main className="min-h-screen">{children}</main>
+            <Footer />
+
         </AuthProvider>
       </body>
     </html>
--- components/praktisch/zorg/AlleZorgInstanties.jsx
+++ components/praktisch/zorg/AlleZorgInstanties.jsx
@@ -11,53 +11,56 @@ const AllZorgInstanties = () => {
     setDropDownId(dropDownId === id ? null : id);
   };
 
-  return (
-     data.map((cat) => (
-        <div key={cat.title}>
-          <div>
-            <button
-              type="button"
-              className="mb-4 flex w-full items-center justify-between rounded-md bg-yellow-700 px-4 py-2 text-lg shadow-lg"
-              onClick={() => toggleDropdown(cat.id)}
-            >
-              <span>{cat.title}</span>
-              <IoIosArrowDown
-                color="white"
-                size={32}
-                className={`${dropDownId === cat.id ? "rotate-180" : ""} transition-all duration-700 ease-in-out`}
-              />
-            </button>
-          </div>
+  return data.map((cat) => (
+    <div key={cat.title}>
+      <div>
+        <button
+          type="button"
+          className="mb-4 flex w-full items-center justify-between rounded-md bg-yellow-700 px-4 py-2 text-lg shadow-lg"
+          onClick={() => toggleDropdown(cat.id)}
+        >
+          <span>{cat.title}</span>
+          <IoIosArrowDown
+            color="white"
+            size={32}
+            className={`${dropDownId === cat.id ? "rotate-180" : ""} transition-all duration-700 ease-in-out`}
+          />
+        </button>
+      </div>
 
-          <div
-            className={`transition-height w-full overflow-hidden duration-700 ease-in-out ${
-              dropDownId === cat.id ? "max-h-[600px] opacity-100" : "max-h-[0px] opacity-0"
-            }`}
-          >
-            <ul className="flex list-disc flex-col gap-4 pl-5 mb-8">
-              {cat.items.map((item, index) => (
-                <li key={index}>
-                <span className="text-xl font-semibold">
-                  {item.naam}
-                </span>
-                <br />
-                {item.adres}
-                <br/>
-                {item.postcode}
-                <br />
-                T: {item.telefoon}
-                <br />
-                Website:{" "}
-                <a href={item.website} target="_blank">
-                  {item.website}
-                </a>
-              </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      ))
-  )
+      <div
+        className={`transition-height w-full overflow-hidden duration-700 ease-in-out ${
+          dropDownId === cat.id
+            ? "max-h-[670px] opacity-100"
+            : "max-h-[0px] opacity-0"
+        }`}
+      >
+        <ul className="mb-8 flex list-disc flex-col gap-4 pl-5">
+          {cat.items.map((item, index) => (
+            <li key={index}>
+              <span className="text-xl font-semibold">{item.naam}</span>
+              {item.adres && <div>{item.adres}</div>}
+              {item.postcode && <div>{item.postcode}</div>}
+              {item.telefoon && <div>T: {item.telefoon}</div>}
+              {item.email && <div>E: {item.email}</div>}
+              {item.website && (
+                <div>
+                  Website:{" "}
+                  <a
+                    href={item.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {item.website}
+                  </a>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  ));
 }
 
 export default AllZorgInstanties
