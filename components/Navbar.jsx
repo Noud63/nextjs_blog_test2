f0d477bbaf7ed85f3c84ae66ab4f64c1f2e12d4f
Noud63 <noudvandun@gmail.com>
2025-01-08 14:26:07 +0100

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,10 +12,10 @@ const Navbar = () => {
         <div className="flex flex-row items-center">
           <Link href="/">
             <div className="flex flex-col tracking-wide">
-              <div className="border font-CloisterBlack text-[4rem] leading-none text-white">
+              <div className="retina:scale-[0.9] border font-CloisterBlack text-[4rem] leading-none text-white">
                 Staatslieden
               </div>
-              <div className="retina:scale-[0.9] text-[.78rem] text-[#ffcb3b]">
+              <div className="text-[.78rem] text-[#ffcb3b]">
                 webapplicatie voor de Staatsliedenbuurt Amsterdam
               </div>
             </div>
