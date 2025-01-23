fa7a5b9aeb45d7fc28803ac153674d6d643fb7bd
Noud63 <noudvandun@gmail.com>
2025-01-23 13:46:51 +0100

    praktisch

--- app/pages/menu/demografie/page.jsx
+++ app/pages/menu/demografie/page.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const DemografiePage = () => {
   return (
-    <div>
-      
+    <div className="mt-8 px-6 text-white max-sm:mt-4 max-sm:px-4">
+      <div className="flex justify-center border-b-2 text-2xl font-semibold tracking-wide">
+        <span className="px-4 pb-4">Demografie</span>
+      </div>
+      <div className="border-b-2 py-8">
+        <p>
+          Hier vindt je de demografische gegevens van de wijk.
+          <br />
+          Maar wat is demografie eigenlijk? <br />
+          Demografie is bevolkingsonderzoek of bevolkingsleer. <br />
+          Demografie onderzoekt hoe een bevolkingsgroep (gemeenschap) is
+          samengesteld.
+          <br />
+          Hierbij wordt gekeken naar bijvoorbeeld leeftijd, geslacht,
+          nationaliteit, etniciteit, opleidingsniveau, burgerlijke staat en
+          beroep. <br />
+          Hierbij heb je ook te maken met sociale wetenschappen als geografie,
+          sociologie, antropologie en geschiedenis, maar ook exacte
+          wetenschappen als statistiek en wiskunde.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DemografiePage
+export default DemografiePage;
--- app/pages/menu/praktisch/horeca/page.jsx
+++ app/pages/menu/praktisch/horeca/page.jsx
@@ -4,8 +4,13 @@ import Horeca from "@/components/praktisch/Horeca";
 const HorecaPage = () => {
   return (
     <div className="mt-8">
-      <div className="rounded-md bg-white py-2 pl-4 text-lg font-semibold text-yellow-900 mb-4">
+      <div className="rounded-md bg-white py-2 pl-4 text-lg font-semibold text-yellow-900">
         <span className="font-bold">Horeca</span>
+      </div>
+      <div className="py-8 pl-4 max-xsm:pl-2">
+        <p>
+          Hier vindt je alle horecagelegenheden zoals restaurants en café's.
+        </p>
       </div>
       <Horeca />
     </div>
